f65cf75aa1d665d78976f7fbe0d33e58c315faea
Powpow Shen <powpowfp@gmail.com>
2014-07-05 10:56:51 +0800

    Add require-css.

--- assets/scripts/require.config.js
+++ assets/scripts/require.config.js
@@ -1,15 +1,18 @@
 // Set up module management
 require.config({
+  // Plugins
+  map: {
+    '*': {
+      'css': 'scripts/libs/css.min'
+    }
+  },
   //By default load any component from assets
   baseUrl: '/',
   paths: {
     'jquery': 'scripts/libs/jquery-1.10.2.min',
     'ember': 'scripts/libs/ember-1.5.0.min',
     'ember-data': 'scripts/libs/ember-data-1.0.0-beta.5.min',
-    'handlebars': 'scripts/libs/handlebars.runtime-1.1.2',
-
-    // Plugins
-    'css': 'scripts/libs/css'
+    'handlebars': 'scripts/libs/handlebars.runtime-1.1.2'
   },
   shim: {
     'jquery': {
